f71890430c2251cfd8b3849a69604efc9a1ad6ba
mohamadrezagharaee27-oss <mohamadrezagharaee27@gmail.com>
2026-06-29 11:46:22 +0330

    Add files via upload

--- media verse/app/__init__.py
+++ media verse/app/__init__.py
@@ -13,15 +13,16 @@ login_manager = LoginManager()
 migrate = Migrate()
 csrf = CSRFProtect()
 
-app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-app.config['SESSION_COOKIE_SECURE'] = True
-
-WTF_CSRF_ENABLED = False
-
 
 def create_app():
     app = Flask(__name__)
     app.config['WTF_CSRF_TIME_LIMIT'] = None
+
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+    app.config['SESSION_COOKIE_SECURE'] = True
+
+    WTF_CSRF_ENABLED = False
+
 
     # Configuration
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-prod')
